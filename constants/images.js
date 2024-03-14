5ec41f63a53c18022784d41f581406f551e689af
ChristianGallien <158533026+ChristianGallien@users.noreply.github.com>
2024-03-14 16:43:21 -0700

    Add files via upload

---
+++ constants/images.js
@@ -0,0 +1,44 @@
+const user1 = require('../assets/images/pficon.png')
+const user2 = require('../assets/images/pficon.png')
+const user3 = require('../assets/images/pficon.png')
+const user4 = require('../assets/images/pficon.png')
+const user5 = require('../assets/images/pficon.png')
+const user6 = require('../assets/images/pficon.png')
+const user7 = require('../assets/images/pficon.png')
+const user8 = require('../assets/images/pficon.png')
+const user9 = require('../assets/images/yi.jpg')
+const user10 = require('../assets/images/gi.webp')
+const user11 = require('../assets/images/ii.png')
+const user12 = require('../assets/images/si.png')
+const user13 = require('../assets/images/fi.jpg')
+const user14 = require('../assets/images/ei.png')
+const user15 = require('../assets/images/wi.webp')  
+const user16 = require('../assets/images/pficon.png')
+const user17 = require('../assets/images/pficon.png')
+const user18 = require('../assets/images/pficon.png')
+const user19 = require('../assets/images/pficon.png')
+const user20 = require('../assets/images/pficon.png')
+
+
+export default {
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6,
+    user7,
+    user8,
+    user9,
+    user10,
+    user11,
+    user12,
+    user13,
+    user14,
+    user15,
+    user16,
+    user17,
+    user18,
+    user19,
+    user20,
+}
---
+++ navigation/BottomTabNavigation.js
@@ -0,0 +1,205 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { COLORS, FONTS } from '../constants'
+import { Contacts, Chats, More, MoreP, Feed, Network } from '../screens'
+import { FontAwesome, Feather, Ionicons, Entypo, AntDesign } from '@expo/vector-icons'
+
+const Tab = createBottomTabNavigator()
+
+const BottomTabNavigation = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarShowLabel: false,
+                headerShown: false,
+                tabBarHideOnKeyboard: true,
+                tabBarStyle: {
+                    position: 'absolute',
+                    backgroundColor: COLORS.blue,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    elevation: 0,
+                    height: 60,
+                },
+            }}
+        >
+            <Tab.Screen
+                name="Contacts"
+                component={Contacts}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <View
+                                style={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                {focused ? (
+                                    <>
+                                        <Text
+                                            style={{
+                                                ...FONTS.body3,
+                                                color: COLORS.white,
+                                            }}
+                                        >
+                                            Home
+                                        </Text>
+                                        <FontAwesome
+                                            name="circle"
+                                            size={12}
+                                            color={COLORS.white}
+                                        />
+                                    </>
+                                ) : (
+                                    <Feather
+                                        name="home"
+                                        size={24}
+                                        color={COLORS.white}
+                                    />
+                                )}
+                            </View>
+                        )
+                    },
+                }}
+            />
+
+            <Tab.Screen
+                name="Network"
+                component={Network}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <View
+                                style={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                {focused ? (
+                                    <>
+                                        <Text
+                                            style={{
+                                                ...FONTS.body3,
+                                                color: COLORS.white,
+                                            }}
+                                        >
+                                            Network
+                                        </Text>
+                                        <FontAwesome
+                                            name="circle"
+                                            size={12}
+                                            color={COLORS.white}
+                                        />
+                                        
+                                    </>
+                                ) : (
+                                    <Ionicons
+                                        name="people-outline"
+                                        size={24}
+                                        color={COLORS.white}
+                                    />
+                                )}
+                            </View>
+                        )
+                    },
+                }}
+            />
+
+            <Tab.Screen
+                name="Feed"
+                component={Feed}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <View
+                                style={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                {focused ? (
+                                    <>
+                                        <Text
+                                            style={{
+                                                ...FONTS.body3,
+                                                color: COLORS.white,
+                                            }}
+                                        >
+                                            Backpack
+                                        </Text>
+                                        <FontAwesome
+                                            name="circle"
+                                            size={12}
+                                            color={COLORS.white}
+                                        />
+                                        
+                                    </>
+                                ) : (
+                                    <Ionicons
+                                        name="bookmarks-outline"
+                                        size={24}
+                                        color={COLORS.white}
+                                    />
+                                )}
+                            </View>
+                        )
+                    },
+                }}
+            />
+
+
+        
+            
+
+            
+
+            <Tab.Screen
+                name="More"
+                component={More}
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <View
+                                style={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                {focused ? (
+                                    <>
+                                        <Text
+                                            style={{
+                                                ...FONTS.body3,
+                                                color: COLORS.white,
+                                            }}
+                                        >
+                                            Profile
+                                        </Text>
+                                        <FontAwesome
+                                            name="circle"
+                                            size={12}
+                                            color={COLORS.white}
+                                        />
+                                    </>
+                                ) : (
+                                    <Feather
+                                        name="user"
+                                        size={24}
+                                        color={COLORS.white}
+                                    />
+                                )}
+                            </View>
+                        )
+                    },
+                }}
+            />
+
+
+        </Tab.Navigator>
+    )
+}
+
+export default BottomTabNavigation
